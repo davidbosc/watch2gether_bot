7640a5d9375e3ae523969d375de63ed3c86b8fbc
David Bosc <davidjnbosc@gmail.com>
2020-05-20 19:26:22 -0500

    Fixed console logging to print bot name

--- watch2gether_bot.py
+++ watch2gether_bot.py
@@ -14,12 +14,12 @@ class MyClient(discord.Client):
             print('{0.author}: {0.content}'.format(message))
             if(len(message.content.strip().split()) <= 1):
                 await message.channel.send(ERROR_MESSAGE)
-                print('{0}: {1}'.format(self, ERROR_MESSAGE))
+                print('{0}: {1}'.format(self.user, ERROR_MESSAGE))
             else:
                 video_url = message.content.strip().split()[1]
                 room_url = watch2gether_api_util.Watch2getherApiUtil().new_room(video_url)
                 await message.channel.send(room_url)
-                print('{0}: {1}'.format(self, room_url))
+                print('{0}: {1}'.format(self.user, room_url))
 
 client = MyClient()
 bot_token = json.load(open('credentials.json'))['bot_token']
